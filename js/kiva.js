77098cee815c064ca8bd3fd5061002cca1015260
rebeccap0 <rebeccap0@users.noreply.github.com>
2016-11-13 13:28:50 -0800

    Update kiva.js

--- js/kiva.js
+++ js/kiva.js
@@ -7,11 +7,10 @@ $(document).ready(function() {
     });
 });
 function displayLenders() {
-	console.log("here");
 	var sortBy = $(".sortby:checked").val(),
 		countryCode = $("#lenderSelect").val();
 	// get a list of vendor ids
-	$.getJSON("http://api.kivaws.org/v1/lenders/search.json?sort_by=" + sortBy + "&country_code="+countryCode, function(result){
+	$.getJSON("https://api.kivaws.org/v1/lenders/search.json?sort_by=" + sortBy + "&country_code="+countryCode, function(result){
 		var lenders = result.lenders,
 			len = lenders.length,
 			lenderIds = "";
@@ -23,7 +22,7 @@ function displayLenders() {
 				}
 			};
 			// display detailed info on each lender
-			$.getJSON("http://api.kivaws.org/v1/lenders/" + lenderIds + ".json", function(result) {
+			$.getJSON("https://api.kivaws.org/v1/lenders/" + lenderIds + ".json", function(result) {
 					var lenders = result.lenders,
 						len = lenders.length,
 						lender, lenderName="", info = "";
